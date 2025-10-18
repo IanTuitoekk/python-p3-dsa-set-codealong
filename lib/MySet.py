556575c8fed870ed9e8632398a0fc76d8c7cf04c
Ian Tuitoek <iantuitoek4@gmail.com>
2025-10-18 12:24:02 +0300

    Refactor MySet class: improve __str__ method and clean up code formatting

--- lib/MySet.py
+++ lib/MySet.py
@@ -7,18 +7,23 @@ class MySet:
 
     def has(self, value):
         return value in self.dictionary
-    
+
+    def __str__(self):
+        set_list = []
+        for key, value in self.dictionary.items():
+            set_list.append(str(key))
+        return f'MySet: {{{",".join(set_list)}}}'
+
     def add(self, value):
         self.dictionary[value] = True # Add a value as a key on the Dictionary
-        return self                   # Return the updated set
-    
+        return self
+
     def delete(self, value):
         self.dictionary.pop(value, None)
         return self
-    
+
     def size(self):
         return len(self.dictionary)
-    
+
     def clear(self):
         self.dictionary.clear()
-        
